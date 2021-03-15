2b80c6241225de30e64708f45300f8f1736e6f48
Funmilola O <funmilolajire@gmail.com>
2021-03-15 13:09:27 +0100

    fix animation error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
 import { Header } from '../components/Header';
-// import { ComingSoon } from '../components/ComingSoon';
-import { Posts } from '../components/Posts';
-import { Subreddits } from '../components/Subreddits';
+import { ComingSoon } from '../components/ComingSoon';
+// import { Posts } from '../components/Posts';
+// import { Subreddits } from '../components/Subreddits';
 import '../scss/main.scss';
 export const App = () => {
     return (
         <div className="App">
             <Header />
-            {/* <ComingSoon /> */}
-            <main>
+            <ComingSoon />
+            {/* <main>
                 <Subreddits />
                 <Posts />
-            </main>
+            </main> */}
         </div>
     )
 }
